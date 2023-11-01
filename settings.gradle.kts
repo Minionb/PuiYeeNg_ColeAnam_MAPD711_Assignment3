c99d6c9b8fd878d8a2b0cf685b565a3a8a82d549
Pui Yee Ng <hilarypyng@gmail.com>
2023-11-01 10:11:38 -0400

    Initialized project files and made search by cities spinner UI components

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PuiYeeNg_ColeAnam_MAPD711_Assignment3"
+include(":app")
+ 
